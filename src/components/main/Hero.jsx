4f76b3f1094996513534f58c8eaa79c4e48982f0
VasylMolnar <vasylmolnar281@gmail.com>
2022-10-18 15:03:40 +0300

    Update

--- src/components/main/Hero.jsx
+++ src/components/main/Hero.jsx
@@ -1,10 +1,10 @@
-//import video from '../../images/Video/video.mp4';
+import video from '../../images/Video/video.mp4';
 
 const Hero = () => {
   return (
     <section className="hero">
       <div className="video-container">
-        {/*<video src={video} className="video" loop autoPlay muted></video>*/}
+        <video src={video} className="video" loop autoPlay muted></video>
       </div>
       <div className="content">
         <h3>Пригода варта того </h3>
