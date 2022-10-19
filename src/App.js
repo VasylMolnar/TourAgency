5fc810f284265d29a398db22d08c68747f53b58c
VasylMolnar <vasylmolnar281@gmail.com>
2022-10-19 20:12:13 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home/HomePages';
-import Gallery from './pages/Gallery/GalleryPages.jsx';
 
 function App() {
   return (
     <>
-      <Lin href="/gallery">Gallery</Lin>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/gallery" element={<Gallery />} />
       </Routes>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './sass/main.scss';
 import { BrowserRouter } from 'react-router-dom';
-import Home from './pages/Home/HomePages';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Home />
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
