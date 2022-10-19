09608946f7179bae8bd9c90e818cf95b26b189f0
VasylMolnar <vasylmolnar281@gmail.com>
2022-10-19 19:40:30 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Gallery from './pages/Gallery/Gallery';
 
 function App() {
   return (
     <div>
       <Header />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/gallery" element={<Gallery />} />
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </div>
   );
