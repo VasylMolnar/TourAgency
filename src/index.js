224b8231054259b82f5dd22488131b3b21fd319f
VasylMolnar <vasylmolnar281@gmail.com>
2022-10-19 18:50:18 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+//import App from './App';
 import './sass/main.scss';
+import Home from './pages/Home/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Home />
     </BrowserRouter>
   </React.StrictMode>
 );
